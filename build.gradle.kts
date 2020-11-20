97979f6a627e5624e0aed61d38a3157307387dfd
Zaur Nasibov <zaur.nasibov@abloy.com>
2020-11-20 12:15:07 +0200

    Add minimal gradle project files

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.71"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("org.junit.jupiter:junit-jupiter:5.6.0")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "renovate-kotlin-gradle-dsl-plugins-bug-showcase"
+
